e9125b43f68c749c9d83fdf3297f37d3595fc078
5um4n7h <53358906+5um4n7h@users.noreply.github.com>
2020-07-15 12:31:31 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import random
+
+print("Number guessing game")
+
+number = random.randint(1, 9)
+
+chances = 0
+
+print("Guess a number (between 1 and 9):")
+while chances < 3:
+    guess = int(input())
+    chances += 1
+    if guess == number:
+        print("Congratulation YOU WON!!!")
+        exit(0)
+    if chances < 3:
+        print("Worng!, Guess agian..")
+
+
+print("YOU LOSE!!! The number is", number)
+exit(0)
+
